0cc000ef05889219d7746a4a2544ed512735f4b3
Daniel Poon <daniel.poon.wenjie@gmail.com>
2019-01-19 18:03:29 +0800

    Add db

---
+++ db.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Connection : Wrapper for managing interactions with DB
+type Connection struct {
+	Type        string
+	DB          *sql.DB
+	QueryString string
+	TypeMap     TypeMap
+}
+
+// Close : Close Connection
+func (c *Connection) Close() error {
+	return c.DB.Close()
+}
+
+// GetBQSchema : Get BigQuery schema for specific table
+func (c *Connection) GetBQSchema(dbName, tableName string) (string, error) {
+	tableSchema := NewTableSchema(c.TypeMap)
+
+	rows, err := c.DB.Query(c.QueryString, dbName, tableName)
+	if err != nil {
+		return "", err
+	}
+
+	for rows.Next() {
+		var columnSchema ColumnSchema
+
+		err = rows.Scan(&columnSchema.Name, &columnSchema.Type)
+		if err != nil {
+			return "", err
+		}
+
+		err = tableSchema.AddColumn(&columnSchema)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return tableSchema.ToJSON()
+}
+
+// NewConnection : Create new Connection
+func NewConnection(connType, username, password, host string) (*Connection, error) {
+	connStr := fmt.Sprintf("%s:%s@tcp(%s)/?charset=utf8", username, password, host)
+
+	db, err := sql.Open(connType, connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	queryStr, ok := SQLQueryMap[connType]
+	if !ok {
+		err := fmt.Errorf("Unable to find query for %s", connType)
+		return nil, err
+	}
+
+	typesMap, ok := SQLTypeMap[connType]
+	if !ok {
+		err := fmt.Errorf("Unable to find types mapping for %s", connType)
+		return nil, err
+	}
+
+	return &Connection{
+		Type:        connType,
+		DB:          db,
+		QueryString: queryStr,
+		TypeMap:     typesMap,
+	}, nil
+}
